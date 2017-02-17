241e4ce776a3ad0c1257172d7d33c10c37030087
nlzz22 <nlzz_22@hotmail.com>
2017-02-17 13:29:02 +0800

    Modified test case for querying number of students in year of study
    this is done to accomodate the count 0 of year 5 and 6 students being returned by the backend code

--- test/test_student_enrollment_queries.py
+++ test/test_student_enrollment_queries.py
@@ -24,7 +24,7 @@ class TestCode(object):
         '''
             Tests querying number of students at each year of study
         '''
-        num_in_year = [4, 3, 3, 3]
+        num_in_year = [4, 3, 3, 3, 0, 0]
 
         table_of_year_of_study_with_count = \
             model.get_num_students_by_yr_study()
@@ -50,7 +50,7 @@ class TestCode(object):
         sql_command = "INSERT INTO student VALUES('D9818872A', 5)"
         model.DB_CURSOR.execute(sql_command)
 
-        num_in_year = [4, 3, 3, 3, 1]
+        num_in_year = [4, 3, 3, 3, 1, 0]
 
         table_of_year_of_study_with_count = \
             model.get_num_students_by_yr_study()
