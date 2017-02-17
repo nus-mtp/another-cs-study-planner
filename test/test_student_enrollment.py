819e13e19e1c5b6398cd925a44a4730510c969f6
Tay Guo Qiang <tayguoqiang1993@gmail.com>
2017-02-17 21:09:12 +0800

    Adjusted student enrollment test case to account for up to year 6 of student candidature

--- test/test_student_enrollment.py
+++ test/test_student_enrollment.py
@@ -44,13 +44,29 @@ class TestCode(object):
         'Software Engineering'
     ]
 
+    # Single-degree students have highest candidature period of 5 years
+    # Double-degree students have highest candidature period of 6 years
     TABLE_YEARS_OF_STUDY = [
         '<td>1</td>',
         '<td>2</td>',
         '<td>3</td>',
-        '<td>4</td>'
+        '<td>4</td>',
+        '<td>5</td>',
+        '<td>6</td>'
     ]
 
+
+    def __init__(self):
+        self.middleware = None
+        self.test_app = None
+
+
+    def setUp(self):
+        '''
+            Sets up the required app instance.
+        '''
+        self.middleware = []
+        self.test_app = TestApp(APP.wsgifunc(*self.middleware))
 
 
     def test_student_enrollment_valid_response(self):
@@ -58,9 +74,7 @@ class TestCode(object):
             Tests whether user can access page for showing student
             enrollment without request errors.
         ''' 
-        middleware = []
-        test_app = TestApp(APP.wsgifunc(*middleware))
-        root = test_app.get(self.URL_STUDENT_ENROLLMENT)
+        root = self.test_app.get(self.URL_STUDENT_ENROLLMENT)
 
         assert_equal(root.status, 200)
 
@@ -70,9 +84,7 @@ class TestCode(object):
             Tests if the student enrollment page contains
             the necessary views.
         '''
-        middleware = []
-        test_app = TestApp(APP.wsgifunc(*middleware))
-        root = test_app.get(self.URL_STUDENT_ENROLLMENT)
+        root = self.test_app.get(self.URL_STUDENT_ENROLLMENT)
 
         root.mustcontain(self.TABLE_HEADER_SENIORITY)
         root.mustcontain(self.TABLE_HEADER_FOCUS_AREA)
@@ -85,9 +97,7 @@ class TestCode(object):
             listing of all focus areas, inclusive of those who
             have not indicated any focus areas.
         '''
-        middleware = []
-        test_app = TestApp(APP.wsgifunc(*middleware))
-        root = test_app.get(self.URL_STUDENT_ENROLLMENT)
+        root = self.test_app.get(self.URL_STUDENT_ENROLLMENT)
 
         root.mustcontain(self.TABLE_FOCUS_AREAS[0],
                          self.TABLE_FOCUS_AREAS[1],
@@ -109,9 +119,7 @@ class TestCode(object):
             Currently assumes that the most senior student is up
             to year 4 of study.
         '''
-        middleware = []
-        test_app = TestApp(APP.wsgifunc(*middleware))
-        root = test_app.get(self.URL_STUDENT_ENROLLMENT)
+        root = self.test_app.get(self.URL_STUDENT_ENROLLMENT)
 
         root.mustcontain(self.TABLE_YEARS_OF_STUDY[0],
                          self.TABLE_YEARS_OF_STUDY[1],
