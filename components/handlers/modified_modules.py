47a11a9ac0a32bdd2e827c29c60a1fdd6889bdd0
xaterz <ruofan.yu@gmail.com>
2017-03-01 17:17:41 +0800

    Implemented viewing of modules with modified mountings

--- components/handlers/modified_modules.py
+++ components/handlers/modified_modules.py
@@ -7,6 +7,8 @@
 from app import RENDER, SESSION
 import web
 from components import model
+from components.handlers.fixed_module_mountings import Fixed
+from components.handlers.tentative_module_mountings import Tentative
 
 
 class Modified(object):
@@ -15,14 +17,27 @@ class Modified(object):
         It retrieves a list of modified modules and determine which attributes
         have been modified.
     '''
-    def GET(self):
+    def __init__(self):
         '''
-            Renders the modified modules page if users requested
-            for the page through the GET method.
+            Define the number of future AYs that will be included
         '''
-        if SESSION['id'] != web.ACCOUNT_LOGIN_SUCCESSFUL:
-            raise web.seeother('/login')
+        self.number_of_future_ays = 1
 
+
+    def get_next_ay(self, ay):
+        '''
+            Return the AY that comes after the given AY
+        '''
+        ay = ay.split(' ')[1].split('/')
+        return 'AY ' + str(int(ay[0])+1) + '/' + str(int(ay[1])+1)
+
+
+    def get_modules_with_modified_quota(self):
+        '''
+            Get all modules whose quota has been modified in a future AY.
+            Return the module code, current AY-Sem, target AY-Sem, current quota, 
+            modified quota, and quota difference
+        '''
         modified_modules = model.get_modules_with_modified_quota()
         modified_modules = [list(module) for module in modified_modules]
         for module in modified_modules:
@@ -35,8 +50,89 @@ class Modified(object):
             else:
                 quota_difference = modified_quota - current_quota
             module.append(quota_difference)
+        return modified_modules
 
-        return RENDER.moduleModified_stub(modified_modules)
+
+    def get_modules_with_modified_mounting(self):
+        '''
+            Get all modules whose mounting has been modified in a future AY.
+            Return the module code, current AY-Sem, target AY-Sem, current quota, 
+            modified quota, and quota difference
+        '''
+        # Generate fixed mounting plan
+        fixed_mounting_handler = Fixed()
+        current_ay = model.get_current_ay()
+        fixed_mounting_handler.populate_module_code_and_name()
+        fixed_mounting_handler.populate_mounting_values()
+        fixed_full_mounting_plan = fixed_mounting_handler.full_mounting_plan
+
+        modified_modules = []
+        target_ay = current_ay
+
+        # Loop through each future AY
+        for i in range(self.number_of_future_ays):
+            target_ay = self.get_next_ay(target_ay)
+
+            # Generate tentative mounting plan
+            tenta_mounting_handler = Tentative()
+            tenta_mounting_handler.populate_module_code_and_name()
+            tenta_mounting_handler.populate_mounting_values(target_ay)
+            tenta_full_mounting_plan = tenta_mounting_handler.full_mounting_plan
+
+            # Compare the fixed and tentative mounting of each module for each semester
+            # to see if there is any difference (if there is, means it's modified)
+            for i in range(len(fixed_full_mounting_plan)):
+                fixed_subplan = fixed_full_mounting_plan[i]
+                tenta_subplan = tenta_full_mounting_plan[i]
+                module_code = fixed_subplan[0]
+
+                fixed_ay_sem = fixed_subplan[1]
+                fixed_sem_1_mounting = fixed_subplan[2]
+                tenta_sem_1_mounting = tenta_subplan[2]
+                fixed_sem_2_mounting = fixed_subplan[3]
+                tenta_sem_2_mounting = tenta_subplan[3]
+
+                if tenta_sem_1_mounting == 0:
+                    modified_modules.append([module_code, current_ay+" Sem 1",
+                                               target_ay+" Sem 1", "Unmounted"])
+                elif tenta_sem_1_mounting == 1 and fixed_sem_1_mounting == -1:
+                    modified_modules.append([module_code, current_ay+" Sem 1",
+                                               target_ay+" Sem 1", "Mounted"])
+
+                if tenta_sem_2_mounting == 0:
+                    modified_modules.append([module_code, current_ay+" Sem 2",
+                                               target_ay+" Sem 2", "Unmounted"])
+                elif tenta_sem_2_mounting == 1 and fixed_sem_2_mounting == -1:
+                    modified_modules.append([module_code, current_ay+" Sem 2",
+                                               target_ay+" Sem 2", "Mounted"])
+
+        return modified_modules
+
+
+    def GET(self):
+        '''
+            Renders the modified modules page if users requested
+            for the page through the GET method.
+        '''
+        if SESSION['id'] != web.ACCOUNT_LOGIN_SUCCESSFUL:
+            raise web.seeother('/login')
+
+        # User can select the type of modified information they want to see
+        # By default, the page will show ALL modified modules
+        modify_type = None
+        try:
+            input_data = web.input()
+            modify_type = input_data.modifyType
+        except Exception:
+            modify_type = "all"
+
+        modified_modules = None
+        if modify_type == "mounting":
+            modified_modules = self.get_modules_with_modified_mounting()
+        elif modify_type == "quota":
+            modified_modules = self.get_modules_with_modified_quota()
+
+        return RENDER.moduleModified_stub(modify_type, modified_modules)
 
 
     def POST(self):
--- test/test_module_modified_backend.py
+++ test/test_module_modified_backend.py
@@ -4,6 +4,7 @@
 '''
 from nose.tools import assert_equal, assert_true, assert_false
 from components import model
+from components.handlers.modified_modules import Modified
 
 
 class TestCode(object):
@@ -11,8 +12,18 @@ class TestCode(object):
         This class runs the test cases related to modified modules.
     '''
     def __init__(self):
-        self.current_ay = "AY 16/17"
-        self.next_ay = "AY 17/18"
+        self.current_ay = model.get_current_ay()
+        self.next_ay = self.get_next_ay(self.current_ay)
+        self.next_next_ay = self.get_next_ay(self.next_ay)
+        self.modified_modules_handler = Modified()
+
+
+    def get_next_ay(self, ay):
+        '''
+            Return the AY that comes after the given AY
+        '''
+        ay = ay.split(' ')[1].split('/')
+        return 'AY ' + str(int(ay[0])+1) + '/' + str(int(ay[1])+1)
 
 
     def setUp(self):
@@ -33,6 +44,13 @@ class TestCode(object):
         model.add_module('BB1006', 'Dummy Module 6',
                          "This module's quota for both sem 1 & 2 have been modified", 6, 'Active')
 
+        model.add_module('BB2001', 'Dummy Module 1',
+                         "This module is unmounted from sem 1", 1, 'Active')
+        model.add_module('BB2002', 'Dummy Module 2',
+                         "This module is remounted in sem 2", 2, 'Active')
+        model.add_module('BB2003', 'Dummy Module 3',
+                         "This module is changed from mounted in sem 1 to sem 2", 3, 'Active')
+
         # Dummy fixed mountings
         model.add_fixed_mounting('BB1001', self.current_ay+' Sem 1', 10)
         model.add_fixed_mounting('BB1001', self.current_ay+' Sem 2', 10)
@@ -47,6 +65,11 @@ class TestCode(object):
         model.add_fixed_mounting('BB1006', self.current_ay+' Sem 1', 60)
         model.add_fixed_mounting('BB1006', self.current_ay+' Sem 2', 60)
 
+        model.add_fixed_mounting('BB2001', self.current_ay+' Sem 1', 10)
+        model.add_fixed_mounting('BB2001', self.current_ay+' Sem 2', 10)
+        model.add_fixed_mounting('BB2002', self.current_ay+' Sem 1', 20)
+        model.add_fixed_mounting('BB2003', self.current_ay+' Sem 1', 30)
+
         # Dummy tentative mountings
         model.add_tenta_mounting('BB1001', self.next_ay+' Sem 1', 10)
         model.add_tenta_mounting('BB1001', self.next_ay+' Sem 2', 10)
@@ -60,6 +83,11 @@ class TestCode(object):
         model.add_tenta_mounting('BB1005', self.next_ay+' Sem 2', None)
         model.add_tenta_mounting('BB1006', self.next_ay+' Sem 1', 999)
         model.add_tenta_mounting('BB1006', self.next_ay+' Sem 2', None)
+
+        model.add_tenta_mounting('BB2001', self.next_ay+' Sem 2', 10)
+        model.add_tenta_mounting('BB2002', self.next_ay+' Sem 1', 20)
+        model.add_tenta_mounting('BB2002', self.next_ay+' Sem 2', 20)
+        model.add_tenta_mounting('BB2003', self.next_ay+' Sem 2', 30)
 
 
     def tearDown(self):
@@ -78,6 +106,11 @@ class TestCode(object):
         model.delete_fixed_mounting('BB1005', self.current_ay+' Sem 2')
         model.delete_fixed_mounting('BB1006', self.current_ay+' Sem 1')
         model.delete_fixed_mounting('BB1006', self.current_ay+' Sem 2')
+        model.delete_fixed_mounting('BB2001', self.current_ay+' Sem 1')
+        model.delete_fixed_mounting('BB2001', self.current_ay+' Sem 2')
+        model.delete_fixed_mounting('BB2002', self.current_ay+' Sem 1')
+        model.delete_fixed_mounting('BB2003', self.current_ay+' Sem 1')
+
         model.delete_tenta_mounting('BB1001', self.next_ay+' Sem 1')
         model.delete_tenta_mounting('BB1001', self.next_ay+' Sem 2')
         model.delete_tenta_mounting('BB1002', self.next_ay+' Sem 1')
@@ -90,6 +123,11 @@ class TestCode(object):
         model.delete_tenta_mounting('BB1005', self.next_ay+' Sem 2')
         model.delete_tenta_mounting('BB1006', self.next_ay+' Sem 1')
         model.delete_tenta_mounting('BB1006', self.next_ay+' Sem 2')
+        model.delete_tenta_mounting('BB2001', self.next_ay+' Sem 2')
+        model.delete_tenta_mounting('BB2002', self.next_ay+' Sem 1')
+        model.delete_tenta_mounting('BB2002', self.next_ay+' Sem 2')
+        model.delete_tenta_mounting('BB2003', self.next_ay+' Sem 2')
+
         model.delete_module('BB1001')
         model.delete_module('BB1002')
         model.delete_module('BB1003')
@@ -105,7 +143,7 @@ class TestCode(object):
         '''
         test_module_code = 'BB1001'
 
-        modified_modules = model.get_modules_with_modified_quota()
+        modified_modules = self.modified_modules_handler.get_modules_with_modified_quota()
         is_in_modified_modules = False
 
         for module in modified_modules:
@@ -128,7 +166,7 @@ class TestCode(object):
         test_target_aysem = self.next_ay+" Sem 1"
         test_modified_quota = 999
 
-        modified_modules = model.get_modules_with_modified_quota()
+        modified_modules = self.modified_modules_handler.get_modules_with_modified_quota()
         is_in_modified_modules = False
         current_aysem = None
         current_quota = -1
@@ -163,7 +201,7 @@ class TestCode(object):
         test_target_aysem = self.next_ay+" Sem 2"
         test_modified_quota = 999
 
-        modified_modules = model.get_modules_with_modified_quota()
+        modified_modules = self.modified_modules_handler.get_modules_with_modified_quota()
         is_in_modified_modules = False
         current_aysem = None
         current_quota = -1
@@ -198,7 +236,7 @@ class TestCode(object):
         test_target_aysem = self.next_ay+" Sem 1"
         test_modified_quota = 999
 
-        modified_modules = model.get_modules_with_modified_quota()
+        modified_modules = self.modified_modules_handler.get_modules_with_modified_quota()
         is_in_modified_modules = False
         current_aysem = None
         current_quota = -1
@@ -233,7 +271,7 @@ class TestCode(object):
         test_target_aysem = self.next_ay+" Sem 2"
         test_modified_quota = None
 
-        modified_modules = model.get_modules_with_modified_quota()
+        modified_modules = self.modified_modules_handler.get_modules_with_modified_quota()
         is_in_modified_modules = False
         current_aysem = None
         current_quota = -1
@@ -269,7 +307,7 @@ class TestCode(object):
         test_target_aysem = self.next_ay+" Sem 1"
         test_modified_quota = 999
 
-        modified_modules = model.get_modules_with_modified_quota()
+        modified_modules = self.modified_modules_handler.get_modules_with_modified_quota()
         is_in_modified_modules = False
         current_aysem = None
         current_quota = -1
@@ -316,3 +354,174 @@ class TestCode(object):
         assert_equal(test_current_quota, current_quota)
         assert_equal(test_target_aysem, target_aysem)
         assert_equal(test_modified_quota, modified_quota)
+
+
+    def test_mounting_not_modified(self):
+        '''
+            Test that a module whose mounting is NOT modified
+            will not appear in the list of modified modules
+        '''
+        test_module_code = 'BB1001'
+
+        modified_modules = self.modified_modules_handler.get_modules_with_modified_mounting()
+        is_in_modified_modules = False
+
+        for module in modified_modules:
+            code = module[0]
+            if code == test_module_code:
+                is_in_modified_modules = True
+                break
+
+        assert_false(is_in_modified_modules)
+
+
+    def test_unmounted_module(self):
+        '''
+            Test that a module whose is unmounted
+            will appear in the list of modified modules
+        '''
+        test_module_code = 'BB2001'
+        test_current_aysem = self.current_ay+" Sem 1"
+        test_target_aysem = self.next_ay+" Sem 1"
+        test_mounting_change = "Unmounted"
+
+        modified_modules = self.modified_modules_handler.get_modules_with_modified_mounting()
+        is_in_modified_modules = False
+        current_aysem = None
+        target_aysem = None
+        mounting_change = None
+
+        for module in modified_modules:
+            code = module[0]
+            if code == test_module_code:
+                is_in_modified_modules = True
+                current_aysem = module[1]
+                target_aysem = module[2]
+                mounting_change = module[3]
+                break
+
+        assert_true(is_in_modified_modules)
+        assert_equal(test_current_aysem, current_aysem)
+        assert_equal(test_target_aysem, target_aysem)
+        assert_equal(test_mounting_change, mounting_change)
+
+
+    def test_remounted_module(self):
+        '''
+            Test that a module whose is remounted
+            will appear in the list of modified modules
+        '''
+        test_module_code = 'BB2002'
+        test_current_aysem = self.current_ay+" Sem 2"
+        test_target_aysem = self.next_ay+" Sem 2"
+        test_mounting_change = "Mounted"
+
+        modified_modules = self.modified_modules_handler.get_modules_with_modified_mounting()
+        is_in_modified_modules = False
+        current_aysem = None
+        target_aysem = None
+        mounting_change = None
+
+        for module in modified_modules:
+            code = module[0]
+            if code == test_module_code:
+                is_in_modified_modules = True
+                current_aysem = module[1]
+                target_aysem = module[2]
+                mounting_change = module[3]
+                break
+
+        assert_true(is_in_modified_modules)
+        assert_equal(test_current_aysem, current_aysem)
+        assert_equal(test_target_aysem, target_aysem)
+        assert_equal(test_mounting_change, mounting_change)
+
+
+    def test_multiple_mounting_changes(self):
+        '''
+            Test that a module whose mounting is modified for multiple AY-Sems
+            will appear as multiple entries in the modified module table
+        '''
+        test_module_code = 'BB2003'
+        test_current_aysem = self.current_ay+" Sem 1"
+        test_target_aysem = self.next_ay+" Sem 1"
+        test_mounting_change = "Unmounted"
+
+        modified_modules = self.modified_modules_handler.get_modules_with_modified_mounting()
+        is_in_modified_modules = False
+        current_aysem = None
+        target_aysem = None
+        mounting_change = None
+
+        current_index = 0
+        for module in modified_modules:
+            code = module[0]
+            if code == test_module_code:
+                is_in_modified_modules = True
+                current_aysem = module[1]
+                target_aysem = module[2]
+                mounting_change = module[3]
+                break
+            current_index += 1
+
+        assert_true(is_in_modified_modules)
+        assert_equal(test_current_aysem, current_aysem)
+        assert_equal(test_target_aysem, target_aysem)
+        assert_equal(test_mounting_change, mounting_change)
+
+        test_current_aysem = self.current_ay+" Sem 2"
+        test_target_aysem = self.next_ay+" Sem 2"
+        test_mounting_change = "Mounted"
+
+        is_in_modified_modules = False
+        for module in modified_modules[current_index+1:]:
+            code = module[0]
+            if code == test_module_code:
+                is_in_modified_modules = True
+                current_aysem = module[1]
+                target_aysem = module[2]
+                mounting_change = module[3]
+                break
+            current_index += 1
+
+        assert_true(is_in_modified_modules)
+        assert_equal(test_current_aysem, current_aysem)
+        assert_equal(test_target_aysem, target_aysem)
+        assert_equal(test_mounting_change, mounting_change)
+
+
+    def test_mounting_modified_in_multiple_ays(self):
+        '''
+            Test that the modified module table can display modules
+            whose mounting is modified for multiple future AYs
+        '''
+        test_module_code = 'BB1001'   # not mounted in next next AY
+        test_current_aysem = self.current_ay+" Sem 1"
+        test_target_aysem = self.next_next_ay+" Sem 1"
+        test_mounting_change = "Unmounted"
+
+        # Set modified module to check for modifications in next two AYs
+        self.modified_modules_handler.number_of_future_ays = 2
+
+        modified_modules = self.modified_modules_handler.get_modules_with_modified_mounting()
+        is_in_modified_modules = False
+        current_aysem = None
+        target_aysem = None
+        mounting_change = None
+
+        for module in modified_modules:
+            code = module[0]
+            if code == test_module_code:
+                is_in_modified_modules = True
+                current_aysem = module[1]
+                target_aysem = module[2]
+                mounting_change = module[3]
+                break
+
+        self.modified_modules_handler.number_of_future_ays = 1
+
+        assert_true(is_in_modified_modules)
+        assert_equal(test_current_aysem, current_aysem)
+        assert_equal(test_target_aysem, target_aysem)
+        assert_equal(test_mounting_change, mounting_change)
+
