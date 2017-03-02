bc00cf42b5716a9df2a83709d24184d839ec24ec
Tay Guo Qiang <tayguoqiang1993@gmail.com>
2017-03-02 17:59:59 +0800

    Added logout functionality

---
+++ components/handlers/logout.py
@@ -0,0 +1,18 @@
+'''
+	This module contains the handler for logging out of the system.
+'''
+
+from app import RENDER
+import web
+from components import model
+
+class Logout(object):
+
+
+	def POST(self):
+		'''
+			This function destroys all cookies related to user session.
+		'''
+		web.ctx.session.kill()
+		web.setcookie('user', '', expires=-1)
+		raise web.seeother('/login')
