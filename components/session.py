274ab0dd98cb2f29706abb568e1ce10732624641
QX <teoqixuan@hotmail.com>
2017-03-07 13:44:08 +0800

    Added methods to support test cases

--- components/session.py
+++ components/session.py
@@ -1,8 +1,16 @@
 '''
-    This module handles session authentication
+    This module handles session authentication and
+    session handling for test cases
 '''
-
+import hashlib
+import uuid
 import web
+import model
+
+DUMMY_USER_ID = 'testUser'
+DUMMY_PASSWORD = '12345678'
+URL_DEFAULT_LOGIN = '/login'
+URL_DEFAULT_LOGOUT = '/logout'
 
 def validate_session():
     '''
@@ -23,3 +31,59 @@ def validate_session():
         return True
     except:
         return False
+
+
+
+    
+def set_up(test_app):
+    '''
+        Sets up for test cases by 
+        creating a dummy user for logging in sessions
+    '''
+    delete_dummy_user_for_tests()
+    create_dummy_user_for_tests()
+    login_session_for_tests(test_app)
+
+def tear_down(test_app):
+    '''
+        Cleans up after one test cases concludes by
+        deleting the dummy user and logging the user out
+    '''
+    delete_dummy_user_for_tests()
+    logout_session_for_tests(test_app)
+
+
+def create_dummy_user_for_tests():
+    '''
+        Creates a dummy valid user that will be 
+        used for creating sessions for test cases.
+    '''
+    salt = uuid.uuid4().hex
+    hashed_password = hashlib.sha512(DUMMY_PASSWORD + salt).hexdigest()
+    model.add_admin(DUMMY_USER_ID, salt, hashed_password)
+
+
+def login_session_for_tests(test_app):
+    '''
+        Logs in the dummy user for test cases
+    '''
+    root = test_app.get(URL_DEFAULT_LOGIN)
+    login_form = root.forms__get()["loginForm"]
+    login_form.__setitem__("username", "user")
+    login_form.__setitem__("password", "12345678")
+    response = login_form.submit()
+
+
+def delete_dummy_user_for_tests():
+    '''
+        Deletes the dummy user used for 
+        creating sessions for test cases.
+    '''
+    model.delete_admin(DUMMY_USER_ID)
+
+
+def logout_session_for_tests(test_app):
+    '''
+        Logs out the dummy user for test cases
+    '''
+    test_app.post(URL_DEFAULT_LOGOUT)
