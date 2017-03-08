043e2d5dfc776cdc81fc5824dd18e3384bf1a724
xaterz <ruofan.yu@gmail.com>
2017-03-08 14:59:11 +0800

    Added modified module populator to adapter

--- components/database_adapter.py
+++ components/database_adapter.py
@@ -36,6 +36,7 @@ def repopulate_database(connection):
         cursor.execute(open("utils/modulePopulator.sql", "r").read())
         cursor.execute(open("utils/moduleMountedPopulator.sql", "r").read())
         cursor.execute(open("utils/moduleMountedTentativePopulator.sql", "r").read())
+        cursor.execute(open("utils/modifiedModulePopulator.sql", "r").read())
         cursor.execute(open("utils/studentAndFocusPopulator.sql", "r").read())
         cursor.execute(open("utils/plannerPopulator.sql", "r").read())
     connection.commit()
