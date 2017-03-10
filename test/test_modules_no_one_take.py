e9928dc42df1805c9e54263d6c04bcd7b818df58
nlzz22 <nlzz_22@hotmail.com>
2017-03-10 19:17:33 +0800

    Modified test case for modules no one take to accept a parameter aysem

--- test/test_modules_no_one_take.py
+++ test/test_modules_no_one_take.py
@@ -29,20 +29,27 @@ class TestCode(object):
             student plan to take together in the same semester
         '''
 
-        list_of_modules_no_one_take = model.get_mods_no_one_take()
+        list_aysem_to_test = ['AY 16/17 Sem 1', 'AY 16/17 Sem 2', 'AY 17/18 Sem 1',
+                              'AY 17/18 Sem 2']
+
         list_of_modules_taken_together = model.get_all_mods_taken_together()
 
-        # Mods that are taken together should be complement of
-        # mods which are not taken together
-        for row in list_of_modules_taken_together:
-            first_mod = row[0]
-            second_mod = row[1]
-            aysem = row[2]
+        for aysem in list_aysem_to_test:
+            list_of_modules_no_one_take = model.get_mods_no_one_take(aysem)
 
-            module_pair_aysem = (first_mod, second_mod, aysem)
+            # Mods that are taken together should be complement of
+            # mods which are not taken together
+            for row in list_of_modules_taken_together:
+                first_mod = row[0]
+                second_mod = row[1]
+                current_aysem = row[2]
+                if current_aysem != aysem:
+                    continue
 
-            assert_false(module_pair_aysem in list_of_modules_no_one_take)
+                module_pair = (first_mod, second_mod)
 
-            alternate_module_pair_aysem = (second_mod, first_mod, aysem)
+                assert_false(module_pair in list_of_modules_no_one_take)
 
-            assert_false(alternate_module_pair_aysem in list_of_modules_no_one_take)
+                alternate_module_pair = (second_mod, first_mod)
+
+                assert_false(alternate_module_pair in list_of_modules_no_one_take)
