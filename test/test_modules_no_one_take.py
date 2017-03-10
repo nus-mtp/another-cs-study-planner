6634a6547339c147a3d86af061f486ff5545d138
nlzz22 <nlzz_22@hotmail.com>
2017-03-10 23:33:43 +0800

    Modified test case for modules no one take to include name of modules

--- test/test_modules_no_one_take.py
+++ test/test_modules_no_one_take.py
@@ -40,16 +40,21 @@ class TestCode(object):
             # Mods that are taken together should be complement of
             # mods which are not taken together
             for row in list_of_modules_taken_together:
-                first_mod = row[0]
-                second_mod = row[1]
+                first_mod_code = row[0]
+                second_mod_code = row[1]
                 current_aysem = row[2]
                 if current_aysem != aysem:
                     continue
 
-                module_pair = (first_mod, second_mod)
+                first_mod_name = model.get_module_name(first_mod_code)
+                second_mod_name = model.get_module_name(second_mod_code)
+
+                module_pair = (first_mod_code, first_mod_name, second_mod_code,
+                               second_mod_name)
 
                 assert_false(module_pair in list_of_modules_no_one_take)
 
-                alternate_module_pair = (second_mod, first_mod)
+                alternate_module_pair = (second_mod_code, second_mod_name, first_mod_code,
+                                         first_mod_name)
 
                 assert_false(alternate_module_pair in list_of_modules_no_one_take)
