b4ff354d14732b7164ce94b11e0cf012661d08ce
nlzz22 <nlzz_22@hotmail.com>
2017-03-10 14:01:25 +0800

    Modified test cases for testing modules taken together for all module pairs to include mod name field

--- test/test_mod_taken_together_queries.py
+++ test/test_mod_taken_together_queries.py
@@ -58,12 +58,18 @@ class TestCode(object):
         list_of_mod_taken_together = \
             model.get_all_mods_taken_together()
 
-        required_list = [('CS1010', 'CS1231', 'AY 16/17 Sem 1', 3),
-                         ('CS1010', 'CS2105', 'AY 16/17 Sem 1', 2),
-                         ('CS1010', 'CS2106', 'AY 16/17 Sem 1', 1),
-                         ('BT5110', 'CS4246', 'AY 16/17 Sem 1', 1),
-                         ('CS1231', 'CS2105', 'AY 16/17 Sem 1', 2),
-                         ('CS1231', 'CS2106', 'AY 16/17 Sem 1', 1)]
+        required_list = [('CS1010', 'Programming Methodology', 'CS1231',
+                          'Discrete Structures', 'AY 16/17 Sem 1', 3),
+                         ('CS1010', 'Programming Methodology', 'CS2105',
+                          'Introduction to Computer Networks', 'AY 16/17 Sem 1', 2),
+                         ('CS1010', 'Programming Methodology', 'CS2106',
+                          'Introduction to Operating Systems', 'AY 16/17 Sem 1', 1),
+                         ('BT5110', 'Data Management and Warehousing', 'CS4246',
+                          'AI Planning and Decision Making', 'AY 16/17 Sem 1', 1),
+                         ('CS1231', 'Discrete Structures', 'CS2105',
+                          'Introduction to Computer Networks', 'AY 16/17 Sem 1', 2),
+                         ('CS1231', 'Discrete Structures', 'CS2106',
+                          'Introduction to Operating Systems', 'AY 16/17 Sem 1', 1)]
 
         assert_equal(len(list_of_mod_taken_together), len(required_list))
         assert_equal(sorted(list_of_mod_taken_together), sorted(required_list))
