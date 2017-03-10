cc3dcede4a4b32b89ef96f5cfb6fc8fe389fe7bf
a0129998 <a0129998@u.nus.edu>
2017-03-11 00:51:12 +0800

    removed old add module form from module_listing

--- components/handlers/module_listing.py
+++ components/handlers/module_listing.py
@@ -15,9 +15,6 @@ class Modules(object):
     '''
     URL_THIS_PAGE = '/modules'
 
-    def __init__(self):
-        self.form = self.create_form()
-
 
     def GET(self):
         '''
@@ -28,22 +25,12 @@ class Modules(object):
             raise web.seeother('/login')
         else:
             module_infos = model.get_all_modules()
-            form = self.form()
-            if SESSION['displayErrorMessage'] is True:
-                SESSION['displayErrorMessage'] = False
-            else:
-                SESSION['keyError'] = False
-                SESSION['deleteError'] = None
-
-            return RENDER.moduleListing(module_infos, form,
-                                        SESSION['keyError'], SESSION['deleteError'])
+            return RENDER.moduleListing(module_infos)
 
 
     def POST(self):
         '''
-            This function is called when the 'Add Module' form is submitted
-            if form input is invalid, reload the page and shows error message besides field.
-            This function might also be called from button links from other pages.
+            This function might be called from button links from other pages.
         '''
 
         # Detects if this function is called from button links from another page.
@@ -53,69 +40,7 @@ class Modules(object):
         # If referred from another page, direct to this page straight away.
         if referrer_page_shortform != self.URL_THIS_PAGE:
             raise web.seeother(self.URL_THIS_PAGE)
-
-        # When 'Add Module' form is submitted
-        form = self.form()
-        # If module add is unsuccessful
-        if not form.validates():
-            modules = model.get_all_modules()
-            return RENDER.moduleListing(modules, form, SESSION['keyError'])
-
-        # else add module to db and refresh page
-        outcome = model.add_module(form.d.code, form.d.name, form.d.description, form.d.mc, 'New')
-        if outcome is False:
-            SESSION['keyError'] = True
-            SESSION['displayErrorMessage'] = True
         raise web.seeother(self.URL_THIS_PAGE)        # load index.html again
-
-
-    def create_form(self):
-        ''' Creates the 'Add Module' form that will appear on the webpage '''
-        code_validation_alphanumeric = web.form.regexp(
-            r"^\w+$", 'Module code should be alphanumeric.')
-
-        name_validation_alphanumeric = web.form.regexp(
-            r"^[\w\s]+$", 'Module name should be alphanumeric.')
-
-        validation_numeric_only = web.form.regexp(
-            r"^\d+$", 'Number of MCs should be a number.')
-
-        module_code_textbox = web.form.Textbox('code',
-                                               web.form.notnull,
-                                               code_validation_alphanumeric,
-                                               post="<br><br>",
-                                               description="Code")
-
-        module_name_textbox = web.form.Textbox('name',
-                                               web.form.notnull,
-                                               name_validation_alphanumeric,
-                                               post="<br><br>",
-                                               description="Name")
-
-        module_description_textarea = web.form.Textarea('description',
-                                                        web.form.notnull,
-                                                        rows="5",
-                                                        cols="55",
-                                                        post="<br><br>",
-                                                        description="Description")
-
-        module_mcs = web.form.Textbox('mc',
-                                      web.form.notnull,
-                                      validation_numeric_only,
-                                      post="<br><br>",
-                                      description="MCs")
-
-        module_form_submit_button = web.form.Button('Add Module',
-                                                    class_="btn btn-primary")
-
-        form = web.form.Form(module_code_textbox,
-                             module_name_textbox,
-                             module_description_textarea,
-                             module_mcs,
-                             module_form_submit_button)
-        return form
-
-
 
 class FlagAsRemoved(object):
     '''
