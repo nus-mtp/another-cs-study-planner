ecc3929502f5aaf6f96d436eb6790a5bd0a02aef
QX <teoqixuan@hotmail.com>
2017-03-12 00:54:47 +0800

    Linted logout.py

--- components/handlers/logout.py
+++ components/handlers/logout.py
@@ -1,18 +1,18 @@
 '''
-	This module contains the handler for logging out of the system.
+    This module contains the handler for logging out of the system.
 '''
 
-from app import RENDER
 import web
-from components import model
 
 class Logout(object):
+    '''
+        Class for logging out
+    '''
 
-
-	def POST(self):
-		'''
-			This function destroys all cookies related to user session.
-		'''
-		web.setcookie('user', '', expires=-1)
-		web.setcookie('session_id', '', expires=-1)
-		raise web.seeother('/login')
+    def POST(self):
+        '''
+            This function destroys all cookies related to user session.
+        '''
+        web.setcookie('user', '', expires=-1)
+        web.setcookie('session_id', '', expires=-1)
+        raise web.seeother('/login')
