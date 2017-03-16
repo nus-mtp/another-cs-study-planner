be8e3591631191d3cfab9c63a1eb8d5649e2820f
nlzz22 <nlzz_22@hotmail.com>
2017-03-16 22:36:03 +0800

    module_view_in_ay_sem.py now can retrieve pre-requisite string data

--- components/handlers/module_view_in_ay_sem.py
+++ components/handlers/module_view_in_ay_sem.py
@@ -89,6 +89,7 @@ class IndividualModule(object):
         self.load_mounting_info(module_code, target_ay_sem)
         is_future_ay = not self.is_current_ay
         overlapping_mod_list = model.get_mod_taken_together_with(module_code)
+        prereq_string = model.get_prerequisite_as_string(module_code)
 
         return RENDER.individualModuleInfo(module_info, is_future_ay,
                                            target_ay_sem, self.mounting_status,
