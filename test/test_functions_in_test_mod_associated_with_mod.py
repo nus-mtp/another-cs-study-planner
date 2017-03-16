d48473477b578d7f2fbcb080ae228dc06cd98560
nlzz22 <nlzz_22@hotmail.com>
2017-03-16 21:36:25 +0800

    Fixed test case functions in test_functions_in_test_mod_associated_with_mod.py
    this is due to a change in the other test case

--- test/test_functions_in_test_mod_associated_with_mod.py
+++ test/test_functions_in_test_mod_associated_with_mod.py
@@ -13,10 +13,7 @@ class TestCode(object):
         in the file test_mod_associated_with_mod.py
     '''
     def __init__(self):
-        self.STRING_AND = "and"
-        self.STRING_OR = "or"
-        self.STRING_OPEN_BRACKET = "("
-        self.STRING_CLOSE_BRACKET = ")"
+        pass
 
 
     def test_functions_in_file_test_mod_associated_with_mod(self):
@@ -24,18 +21,17 @@ class TestCode(object):
             This function tests if equality function in test_mod_associated_with_mod.py
             is working properly or not.
         '''
-        assert_true(file_to_test.is_prereq_equal(self, "C and (A or B)", "C and (B or A)"))
-        assert_true(file_to_test.is_prereq_equal(self, "A", "A"))
-        assert_true(file_to_test.is_prereq_equal(self, "A or B or C", "B or A or C"))
-        assert_true(file_to_test.is_prereq_equal(self, "A and B", "B and A"))
-        assert_true(file_to_test.is_prereq_equal(self, "C and (A or B)", "(A or B) and C"))
-        assert_true(file_to_test.is_prereq_equal(self, "C and (A or B)", "(B or A) and C"))
-        assert_false(file_to_test.is_prereq_equal(self, "C and (A or B)", "(B or C) and C"))
-        assert_true(file_to_test.is_prereq_equal(self,
-                                                 "(C or D) and (A or B)", "(B or A) and (D or C)"))
-        assert_false(file_to_test.is_prereq_equal(self, "C and (A or B)", "(A or B)"))
-        assert_false(file_to_test.is_prereq_equal(self, "C and (A or B)", "A or B"))
-        assert_false(file_to_test.is_prereq_equal(self, "C", "B"))
-        assert_false(file_to_test.is_prereq_equal(self, "C", "B and C"))
-        assert_false(file_to_test.is_prereq_equal(self, "C", "B or C"))
-        assert_false(file_to_test.is_prereq_equal(self, "C or A", "C and A"))
+        assert_true(file_to_test.is_prereq_equal("C and (A or B)", "C and (B or A)"))
+        assert_true(file_to_test.is_prereq_equal("A", "A"))
+        assert_true(file_to_test.is_prereq_equal("A or B or C", "B or A or C"))
+        assert_true(file_to_test.is_prereq_equal("A and B", "B and A"))
+        assert_true(file_to_test.is_prereq_equal("C and (A or B)", "(A or B) and C"))
+        assert_true(file_to_test.is_prereq_equal("C and (A or B)", "(B or A) and C"))
+        assert_false(file_to_test.is_prereq_equal("C and (A or B)", "(B or C) and C"))
+        assert_true(file_to_test.is_prereq_equal("(C or D) and (A or B)", "(B or A) and (D or C)"))
+        assert_false(file_to_test.is_prereq_equal("C and (A or B)", "(A or B)"))
+        assert_false(file_to_test.is_prereq_equal("C and (A or B)", "A or B"))
+        assert_false(file_to_test.is_prereq_equal("C", "B"))
+        assert_false(file_to_test.is_prereq_equal("C", "B and C"))
+        assert_false(file_to_test.is_prereq_equal("C", "B or C"))
+        assert_false(file_to_test.is_prereq_equal("C or A", "C and A"))
