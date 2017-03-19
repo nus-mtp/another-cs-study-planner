32364e8e3992234e61f8b9749fd3a2cae3ee5cd3
a0129998 <a0129998@u.nus.edu>
2017-03-19 17:52:08 +0800

    added description

--- test/test_students_affected_by_module.py
+++ test/test_students_affected_by_module.py
@@ -15,6 +15,7 @@ class TestCode(object):
 
     URL_NORMAL = '/studentsAffectedByModule?code=IT1005&aysem=AY+17%2F18+Sem+1'
     CONTENT_TITLE = 'Students Affected By Module Changes'
+    CONTENT_DESCRIPTION = '<p>Students who are planning to take this module.</p>'
     CONTENT_SUBTITLE = 'For <b>IT1005</b> in <b>AY 17/18 Sem 1</b>'
 
 
@@ -54,3 +55,4 @@ class TestCode(object):
         root = self.test_app.get(self.URL_NORMAL)
         root.mustcontain(self.CONTENT_TITLE)
         root.mustcontain(self.CONTENT_SUBTITLE)
+        root.mustcontain(self.CONTENT_DESCRIPTION)
