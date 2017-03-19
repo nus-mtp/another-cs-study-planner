8659d9211215f9b250567e9eba09e8c74fd9b497
nlzz22 <nlzz_22@hotmail.com>
2017-03-19 22:56:43 +0800

    removed unused instances of prereq and preclusions string

--- components/handlers/module_view_in_ay_sem.py
+++ components/handlers/module_view_in_ay_sem.py
@@ -119,14 +119,12 @@ class IndividualModule(object):
         is_future_ay = not self.is_current_ay
 
         overlapping_mod_list = model.get_mod_taken_together_with(module_code)
-        prereq_string = model.get_prerequisite_as_string(module_code)
-        preclude_string = model.get_preclusion_as_string(module_code)
         is_starred = model.is_module_starred(module_code, web.cookies().get('user'))
 
         return RENDER.individualModuleInfo(module_info, is_future_ay,
                                            target_ay_sem, self.mounting_status,
                                            self.quota, overlapping_mod_list,
-                                           is_starred, prereq_string, preclude_string)
+                                           is_starred)
 
 
     def POST(self):
