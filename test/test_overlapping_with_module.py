b09f34bc9b66ee9f83cd2375223af14a091e48bf
a0129998 <a0129998@u.nus.edu>
2017-03-22 15:33:55 +0800

    fixed issue #68

--- test/test_overlapping_with_module.py
+++ test/test_overlapping_with_module.py
@@ -11,14 +11,25 @@ class TestCode(object):
     '''
         tests if the individual module overlapping table contains what is should
     '''
-    URL_NORMAL = '/overlappingWithModule?code=BT5110&aysem=AY+17%2F18+Sem+1'
+    URL_NORMAL_GENERAL = '/overlappingWithModule?code=BT5110'
+    URL_NORMAL_SPECIFIC = '/overlappingWithModule?code=BT5110&aysem=AY+17%2F18+Sem+1'
     CONTENT_OVERLAPPING_MODULES_TABLE = '<table id="common-module-table" ' +\
                                         'class="table table-bordered table-hover display ' +\
                                         'dataTable">'
-    CONTENT_OVERLAPPING_MODULES_TITLE = '<h1 class="text-center">Modules Overlapping with '+\
+    CONTENT_OVERLAPPING_MODULES_TITLE_SPECIFIC = '<h1 class="text-center">Modules'+\
+                                                 ' Overlapping with '+\
                                         '<b>BT5110</b> for <b>AY 17/18 Sem 1</b></h1>'
-    CONTENT_OVERLAPPING_MODULES_DESCRIPTION = '<p class="text-center">Shows all modules taken'+\
-                                              ' together with this module at a specified semester.</p>'
+    CONTENT_OVERLAPPING_MODULES_TITLE_GENERAL = '<h1 class="text-center">Modules'+\
+                                                ' Overlapping with '+\
+                                        '<b>BT5110</b> for <b>All Semesters</b></h1>'
+    CONTENT_OVERLAPPING_MODULES_DESCRIPTION_SPECIFIC = '<p class="text-center">'+\
+                                                       'Shows all modules taken'+\
+                                              ' together with this module at a '+\
+                                              'specified semester.</p>'
+    CONTENT_OVERLAPPING_MODULES_DESCRIPTION_GENERAL = '<p class="text-center">Shows '+\
+                                                      'all modules taken'+\
+                                                      ' together with this module in '+\
+                                                      'all semesters.</p>'
     CONTENT_OVERLAPPING_MODULES_TABLE_MODULE = '<th>Module Code</th>'
     CONTENT_OVERLAPPING_MODULES_TABLE_MODULE_NAME = '<th>Module Name</th>'
     CONTENT_OVERLAPPING_MODULES_TABLE_AY_SEM = '<th>For AY-Sem</th>'
@@ -44,23 +55,44 @@ class TestCode(object):
         '''
         session.tear_down(self.test_app)
 
-    def test_overlapping_with_module_response(self):
+    def test_overlapping_with_module_response_general(self):
         '''
             Tests whether user can access page for overlapping module
         '''
-        root = self.test_app.get(self.URL_NORMAL)
+        root = self.test_app.get(self.URL_NORMAL_GENERAL)
         assert_equal(root.status, 200)
 
-    def test_overlapping_with_module_contents(self):
+    def test_overlapping_with_module_response_specific(self):
         '''
-            Tests if the page contains expected contents
+            Tests if user can access page for overlapping with module in ay sem
         '''
-        root = self.test_app.get(self.URL_NORMAL)
+        root = self.test_app.get(self.URL_NORMAL_SPECIFIC)
+        assert_equal(root.status, 200)
+
+    def test_overlapping_with_module_general_contents(self):
+        '''
+            Tests if the page contains expected contents when accessing with module
+        '''
+        root = self.test_app.get(self.URL_NORMAL_GENERAL)
 
         root.mustcontain(self.CONTENT_OVERLAPPING_MODULES_TABLE)
         root.mustcontain(self.CONTENT_OVERLAPPING_MODULES_TABLE_MODULE)
         root.mustcontain(self.CONTENT_OVERLAPPING_MODULES_TABLE_MODULE_NAME)
         root.mustcontain(self.CONTENT_OVERLAPPING_MODULES_TABLE_AY_SEM)
         root.mustcontain(self.CONTENT_OVERLAPPING_MODULES_TABLE_NUM_STUDENTS)
-        root.mustcontain(self.CONTENT_OVERLAPPING_MODULES_TITLE)
-        root.mustcontain(self.CONTENT_OVERLAPPING_MODULES_DESCRIPTION)
+        root.mustcontain(self.CONTENT_OVERLAPPING_MODULES_TITLE_GENERAL)
+        root.mustcontain(self.CONTENT_OVERLAPPING_MODULES_DESCRIPTION_GENERAL)
+
+    def test_overlapping_with_module_specific_contents(self):
+        '''
+            Tests if the page contains expected contents when accessing module of aysem
+        '''
+        root = self.test_app.get(self.URL_NORMAL_SPECIFIC)
+
+        root.mustcontain(self.CONTENT_OVERLAPPING_MODULES_TABLE)
+        root.mustcontain(self.CONTENT_OVERLAPPING_MODULES_TABLE_MODULE)
+        root.mustcontain(self.CONTENT_OVERLAPPING_MODULES_TABLE_MODULE_NAME)
+        root.mustcontain(self.CONTENT_OVERLAPPING_MODULES_TABLE_AY_SEM)
+        root.mustcontain(self.CONTENT_OVERLAPPING_MODULES_TABLE_NUM_STUDENTS)
+        root.mustcontain(self.CONTENT_OVERLAPPING_MODULES_TITLE_SPECIFIC)
+        root.mustcontain(self.CONTENT_OVERLAPPING_MODULES_DESCRIPTION_SPECIFIC)
