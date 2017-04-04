9b41626d31ab236ccab9b0a060ffaec655959209
Tay Guo Qiang <tayguoqiang1993@gmail.com>
2017-04-04 14:55:00 +0800

    Modified edit-prerequities and edit-preclusions test cases to reflect change in UI flow

--- test/test_edit_module_prerequisites.py
+++ test/test_edit_module_prerequisites.py
@@ -50,6 +50,15 @@ class TestCode(object):
                                               'onclick="revertChangesPrerequisite()">' +\
                                               '<span class="glyphicon glyphicon-refresh">' +\
                                               '</span></button>'
+    EDIT_MODULE_PREREQUISITES_BACK_BUTTON = '<a class="btn btn-lg btn-primary" ' +\
+                                            'id="prerequisite-back-button" ' +\
+                                            'href="/editModule?code=BT5110" ' +\
+                                            'data-toggle="tooltip" data-placement="top" ' +\
+                                            'title="Go Back to Editing BT5110">' +\
+                                            '<span class="glyphicon glyphicon-arrow-left">' +\
+                                            '</span></a>'
+
+    EDIT_MODULE_CORRECT_REDIRECT = 'Edit <b>General Information</b> For <b>BT5110</b>'
 
 
     def __init__(self):
@@ -88,6 +97,18 @@ class TestCode(object):
         root.mustcontain(self.EDIT_MODULE_PREREQUISITES_DELETE_UNIT_BUTTON)
         root.mustcontain(self.EDIT_MODULE_PREREQUISITES_SAVE_BUTTON)
         root.mustcontain(self.EDIT_MODULE_PREREQUISITES_REVERT_BUTTON)
+        root.mustcontain(self.EDIT_MODULE_PREREQUISITES_BACK_BUTTON)
+
+
+    def test_module_prereq_edit_back_button(self):
+        '''
+            Tests if the back button for edit-module-prerequisites page works.
+        '''
+        root = self.test_app.get(self.URL_EDIT_MODULE_PREREQ_SPECIFIC_VALID)
+        response = root.click(linkid="prerequisite-back-button")
+
+        assert_equal(response.status, 200)
+        response.mustcontain(self.EDIT_MODULE_CORRECT_REDIRECT)
 
 
     @raises(Exception)
